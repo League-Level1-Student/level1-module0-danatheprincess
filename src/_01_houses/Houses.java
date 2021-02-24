f7ba7dbfa2407cfe841971952f7fe4ac086ff54d
student <student@ip-172-31-62-102.us-west-2.compute.internal>
2021-02-23 18:30:59 -0800

    working on Houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,52 @@
 package _01_houses;
 
+import java.awt.Color;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	Robot rob=new Robot();
+	Random ran=new Random();
 	public void run() {
+			
+			rob.setWindowSize(650, 400);
+			rob.miniaturize();
+			rob.setX(40);
+			rob.setY(375);
+			rob.setSpeed(10);
+			rob.penDown();
+			for(int i=0;i<9;i++) {
+				int number=ran.nextInt(3);
+				if(number==0) {
+					drawHouse("small",Color.BLUE);
+				}else if(number==1) {
+					drawHouse("medium",Color.red);
+				}else {
+					drawHouse("large",Color.cyan);
+				}
+				
+			}
+	}
+	void drawHouse(int height,Color color) {
+		rob.setPenColor(color);
+		rob.setAngle(0);
+		rob.move(height);
+		rob.turn(90);
+		rob.move(25);
+		rob.turn(90);
+		rob.move(height);
+		rob.turn(-90);
+		rob.setPenColor(Color.GREEN);
+		rob.move(15);
 		
+	}
+	void drawHouse(String height,Color color) {
+		if(height.equals("small")){
+			drawHouse(60,color);
+		}else if(height.equals("medium")) {
+			drawHouse(120,color);
+		}else
+			drawHouse(250,color);
 	}
 }
