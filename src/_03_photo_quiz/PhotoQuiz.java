8ff1d6d1f59ffa88bcaa7d951e9601df38751d10
student <student@ip-172-31-58-232.us-west-2.compute.internal>
2021-03-09 18:28:41 -0800

    working on photo quiz

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,26 +26,31 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-String jpg;
-		// 2. create a variable of type "Component" that will hold your image
-
+String image="https://www.planetware.com/wpimages/2019/10/switzerland-in-pictures-most-beautiful-places-matterhorn.jpg";
+// 2. create a variable of type "Component" that will hold your image
+Component picture;
 		// 3. use the "createImage()" method below to initialize your Component
-
+picture=createImage(image);
 		// 4. add the image to the quiz window
-
+quizWindow.add(picture);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String question=JOptionPane.showInputDialog("Where is this mountain located?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(question.equalsIgnoreCase("Switzerland")) {
+	JOptionPane.showMessageDialog(null, "CORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	JOptionPane.showMessageDialog(null,"INCORRECT, better LUCK next time");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+quizWindow.remove(picture);
 		// 10. find another image and create it (might take more than one line
 		// of code)
+String different="https://cdn.pixabay.com/photo/2015/02/24/15/41/wolf-647528__340.jpg";
 
 		// 11. add the second image to the quiz window
 
