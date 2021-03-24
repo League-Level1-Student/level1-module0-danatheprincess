28fd215ebb84ea81d02e3f6a8d08389f15aea55b
student <student@ip-172-31-62-50.us-west-2.compute.internal>
2021-03-23 18:32:37 -0700

    working on Dueling buttons

--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -42,7 +42,7 @@ public class BodyPartQuiz {
 
 		// 3. Change the size of the window so that you can only see part of the
 		// image.
-		window.setSize(500, 500);
+		window.setSize(500, 150);
 
 		showNextImage();
 
@@ -51,7 +51,7 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+int score=0;
 		// 2. Set the size of the window in the initializeGui() method 
 
 		// 4. Ask the user who this person is and store their answer
@@ -59,17 +59,44 @@ public class BodyPartQuiz {
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
-
+if(guess.equalsIgnoreCase("Arnold")) {
+	JOptionPane.showMessageDialog(null, "CORRECT!!!");
+	score+=1;
+}
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
-
+else {
+	JOptionPane.showMessageDialog(null,"WRONG!!, the RIGHT answer is Arnold" );
+}
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
 
 		// 8. .... repeat 4-7 for all your images.....
-
+		guess = JOptionPane.showInputDialog("who is this?");
+		if(guess.equalsIgnoreCase("Leonardo")){
+			JOptionPane.showMessageDialog(null, "CORRECT!!!");
+			score+=1;
+		}else{
+			JOptionPane.showMessageDialog(null,"WRONG!!!, the RIGHT answer is Leonardo");
+		}
+		showNextImage();
+		guess = JOptionPane.showInputDialog("who is this?");
+		if(guess.equalsIgnoreCase("Morgan")) {
+			JOptionPane.showMessageDialog(null, "CORRECT!!!");
+			score+=1;
+		}else {
+			JOptionPane.showMessageDialog(null, "WRONG!!!!, the RIGHT answer is Morgan");
+		}
+		showNextImage();
+		guess = JOptionPane.showInputDialog("who is this?");
+		if(guess.equalsIgnoreCase("Jack")) {
+		JOptionPane.showMessageDialog(null, "CORRECT!!!");
+		score+=1;
+		}else {
+			JOptionPane.showMessageDialog(null,"WRONG!!!, the RIGHT answer is Jack" );
+		}
 		// 9. Show them their current score
-
+JOptionPane.showMessageDialog(null, "Your SCORE is "+ score);
 	}
 
 	public void showNextImage() {
